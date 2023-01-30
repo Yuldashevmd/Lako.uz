9a174f024ee07873407f35d7065743ff9c7323d5
Muhammad <muxammad_205@mail.ru>
2023-01-30 15:31:30 +0500

    footer

--- js/main.js
+++ js/main.js
@@ -35,42 +35,60 @@ const userPhone = document.querySelector("#phone");
 const userComment = document.querySelector("#comment");
 const formSubmit = document.querySelector("#submit");
 const message = document.querySelector("#message");
-
+const toast = document.querySelector(".toast");
 let newUser = {};
 
 // get-values-from-inputs
 formSubmit.addEventListener("click", (e) => {
-  e.preventDefault();
-  formSubmit.setAttribute("disabled", true);
+  if (userName.value === "" || userPhone.value === "") {
+    toast.style.display = "flex";
+    toast.classList.add("toast-warning");
+    toast.textContent =
+      "Ism yoki telefon raqam notog'ri kirildi, iltimos qayta urinib ko'ring...";
+    setTimeout(() => {
+      toast.style.display = "none";
+      toast.classList.remove("toast-warning");
+    }, 2000);
+  } else {
+    e.preventDefault();
+    formSubmit.setAttribute("disabled", true);
+    toast.classList.add("toast-success");
+    toast.style.display = "flex";
+    toast.textContent =
+      "Ma'lumotlar yuborildi! tez orada siz bilan bog'lanamiz";
+    setTimeout(() => {
+      toast.style.display = "none";
+      toast.classList.remove("toast-success");
+    }, 2000);
+    newUser = {
+      name: userName.value,
+      phone: userPhone.value,
+      comment: userComment.value ?? "",
+    };
+    fetch("lako/calluser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res, "res"))
+      .catch((err) => console.log(err, "err"));
+    message.style.color = "green";
+    message.textContent = "Запрос отправлено...";
 
-  newUser = {
-    name: userName.value,
-    phone: userPhone.value,
-    comment: userComment.value ?? "",
-  };
-  fetch("lako/calluser", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify(newUser),
-  })
-    .then((res) => res.json())
-    .then((res) => console.log(res, "res"))
-    .catch((err) => console.log(err, "err"));
-  message.style.color = "green";
-  message.textContent = "Запрос отправлено...";
-
-  setTimeout(() => {
-    userName.value = "";
-    userPhone.value = "";
-    userComment.value = "";
-    message.textContent = "";
-  }, 1500);
-  setTimeout(() => {
-    formSubmit.disabled = false;
-  }, 2500);
-  console.log(newUser);
+    setTimeout(() => {
+      userName.value = "";
+      userPhone.value = "";
+      userComment.value = "";
+      message.textContent = "";
+    }, 1500);
+    setTimeout(() => {
+      formSubmit.disabled = false;
+    }, 2500);
+    console.log(newUser);
+  }
 });
 // form end

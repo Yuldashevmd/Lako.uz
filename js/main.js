c44addbb4ad73c1e0a86b7662ec2f742f818946e
Muhammad <muxammad_205@mail.ru>
2023-01-31 15:20:35 +0500

    lll

--- js/main.js
+++ js/main.js
@@ -35,7 +35,16 @@ document.querySelector(".prev-slide").addEventListener("click", function () {
 });
 
 // carousel end
-
+// number start
+document.getElementById("phone").addEventListener("input", function (e) {
+  var x = e.target.value
+    .replace(/\D/g, "")
+    .match(/(\d{0,2})(\d{0,3})(\d{0,4})/);
+  e.target.value = !x[2]
+    ? x[1]
+    : "(" + x[1] + ") " + x[2] + (x[3] ? "-" + x[3] : "");
+});
+// number end
 // form start
 const userName = document.querySelector("#name");
 const userPhone = document.querySelector("#phone");
